aa4c23a372293d73fe08349f69fed8dd5b4f3ae0
798382 <798382@ELF8IT221517.ACDM.DS.SAIT.CA>
2020-10-27 11:39:57 -0600

    Added into RoleDB

--- User Manager/src/java/dataaccess/RoleDB.java
+++ User Manager/src/java/dataaccess/RoleDB.java
@@ -5,10 +5,40 @@
  */
 package dataaccess;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import models.Role;
+
 /**
  *
  * @author 819466
  */
 public class RoleDB {
-    
+    public List<Role> getAll(String owner) throws Exception {
+        List<Role> role = new ArrayList<>();
+        ConnectionPool cp = ConnectionPool.getInstance();
+        Connection con = cp.getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        String sql = "SELECT * FROM note WHERE owner=?";
+        
+     try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, owner);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+              //do something when Role.java is completed
+            }
+        } finally {
+           DBUtil.closeResultSet(rs);  
+           DBUtil.closePreparedStatement(ps);  
+            cp.freeConnection(con);
+        }
+
+        return role;
+}
 }
