87650551a1564d601ef25c97fc938371a345fac7
Andrew <redma@DESKTOP-CBGG122.cg.shawcable.net>
2020-10-27 11:49:18 -0600

    Userdb get all select statement

--- User Manager/src/java/dataaccess/UserDB.java
+++ User Manager/src/java/dataaccess/UserDB.java
@@ -23,10 +23,15 @@ public class UserDB {
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
+        String sql = "SELECT * from user where email=?";
         try{
             ps = con.prepareStatement(sql);
-            
-            
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+            while(rs.next())
+            {
+               
+            }
         }
     }
 }
