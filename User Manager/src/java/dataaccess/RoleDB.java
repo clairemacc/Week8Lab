98aecb90320e8e6b558819ad0c46846de759a269
798382 <798382@ELF8IT221517.ACDM.DS.SAIT.CA>
2020-10-28 08:26:04 -0600

    Fixed RoleDB with some variable names.

--- User Manager/src/java/dataaccess/RoleDB.java
+++ User Manager/src/java/dataaccess/RoleDB.java
@@ -9,24 +9,24 @@ import models.Role;
 
 public class RoleDB {
 
-    public List<Role> getAll(String owner) throws Exception {
+    public List<Role> getAll(Integer id) throws Exception {
         List<Role> roles = new ArrayList<>();
         ConnectionPool cp = ConnectionPool.getInstance();
         Connection con = cp.getConnection();
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        String sql = "SELECT * FROM note WHERE owner=?";
+        String sql = "SELECT * FROM roles WHERE id=?";
 
         try {
             ps = con.prepareStatement(sql);
-            ps.setString(1, owner);
+            ps.setInt(1, id);
             rs = ps.executeQuery();
             while (rs.next()) {
-                int noteId = rs.getInt(1);
+                int idNum = rs.getInt(1);
                 String name = rs.getString(2);
 
-                Role role = new Role(noteId, name);
+                Role role = new Role(idNum, name);
                 roles.add(role);
             }
         } finally {
